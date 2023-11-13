5ce6a6fdad095c3e7906a9dd8b0c1947cb24db5c
Deej C <deejc204@gmail.com>
2023-11-13 09:54:52 -0700

    keyboard control update

--- KeyboardControl.m
+++ KeyboardControl.m
@@ -3,6 +3,8 @@ InitKeyboard();
 
 while 1
     pause(0.1);
+    brick.MoveMotor('AD', 0)
+    brick.MoveMotor('C', 0)
     switch key
         case 'uparrow'
             brick.MoveMotor('AD', 50);
@@ -12,8 +14,13 @@ while 1
             brick.MoveMotor('D', 50)
         case 'leftarrow'
             brick.MoveMotor('A', 50)
+        case 'w'
+            brick.MoveMotor('C', 2)
+        case 's'
+            brick.MoveMotor('C', -2)
         case 'q'
             break
+        
     end
 end
 
